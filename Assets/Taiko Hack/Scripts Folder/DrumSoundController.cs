fefe078a94e47d44430336f13b842f7c3af97066
StevenHernandez-code <60796760+StevenHernandez-code@users.noreply.github.com>
2024-01-26 17:05:19 -0700

    Drum integration testing + stick model

---
+++ Assets/Taiko Hack/Scripts Folder/DrumSoundController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DrumSoundController : MonoBehaviour
+{
+
+    private AudioSource soundClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundClip = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        soundClip.Play();
+    }
+}
